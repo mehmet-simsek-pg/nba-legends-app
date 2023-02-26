95595dab500654212726b62fe1b281757e893985
Mehmet Simsek <simsekmehmet00@gmail.com>
2023-02-26 02:00:49 +0100

    adapt cors

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ require("dotenv").config();
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 // middleware
 app.use(express.json());
 
