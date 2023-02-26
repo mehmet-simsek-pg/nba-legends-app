62acdc298980935b2ce85dd8de1f7a10d343baf9
Mehmet Simsek <simsekmehmet00@gmail.com>
2023-02-26 02:24:24 +0100

    add cors config

--- server/server.js
+++ server/server.js
@@ -8,14 +8,19 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-  port: 10000,
-};
-
-app.use(cors(corsOptions)); // Use this after the variable declaration
+// Enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 // middleware
 app.use(express.json());
@@ -25,14 +30,14 @@ app.use("/api/v1", players);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
-const portLocal = 5000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
     mongoose.set("strictQuery", false);
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
-      console.log(`Server is listening on port ${portLocal}...`)
+      console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
     console.log(error);
